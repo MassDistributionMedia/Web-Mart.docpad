04445313e1b8dc7bef20a416ef2f95e734dbb4fc
Michael Duane Mooring <mike@mdm.cm>
2013-01-25 22:39:24 -0500

    First Modal!

--- out/scripts/script.js
+++ out/scripts/script.js
@@ -1,13 +1,5 @@
 (function() {
 
-  YUI().use("node", "event", function(Y) {
-    Y.one(".one-time-bubble").hide();
-    Y.one(".one-time-599").on("mouseenter", function() {
-      return Y.one(".one-time-bubble").show();
-    });
-    return Y.one(".one-time-599").on("mouseleave", function() {
-      return Y.one(".one-time-bubble").hide();
-    });
-  });
+
 
 }).call(this);
--- src/documents/scripts/script.js.coffee
+++
@@ -1,8 +0,0 @@
-YUI().use "node", "event", (Y) ->
-
-	Y.one(".one-time-bubble").hide()
-
-	Y.one(".one-time-599").on "mouseenter", ->
-		Y.one(".one-time-bubble").show()
-	Y.one(".one-time-599").on "mouseleave", ->
-		Y.one(".one-time-bubble").hide()
